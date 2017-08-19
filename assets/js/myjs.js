4c29285be501aecca66ca434c05d61eb25ff8bd6
laravelcoder <pmadsen2013@gamil.comgit>
2017-08-19 00:22:00 -0600

    update

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -2,36 +2,8 @@
 
 
 
-function myFunction() {
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
-}
 
 
 
-!(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-104942085-1', 'auto');
-ga('send', 'pageview');
 
-$(document).ready(function() {
-    $('#example').DataTable( {
-        "ajax": '/kaw-reference/master/assets/ajax/data/data.json'
-    } );
-} );
